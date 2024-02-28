e91fe7ef9483e37f28275cf0ab8d416ebde7fdab
Ilya <ilya.krasnenkov@mail.ru>
2024-02-28 09:50:05 +0300

    Решены задачи Бинарное дерево + Реализация класса списка

--- js-homework/bTree.js
+++ js-homework/bTree.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Простой html</title>
-    </head>
-    <body>
-        <script>
-// Бинарное дерево
-
 class Node {
     constructor(data) {
         this.data = data;
@@ -101,62 +92,4 @@ class BinaryTree {
         }
     }
 }
-        </script>
 
-        <script>
-
-class newList {
-    constructor(){
-        this.length = 0
-        this.listValue = {}
-    }
-
-    count(){
-        return this.length
-    }
-
-    add(value){
-        this.listValue[this.length] = value
-        this.length++
-        return this.listValue
-    }
-
-    remove(index){
-        if (index > this.length || index < 0) {
-            return console.log("Incorrect index")
-        }
-
-        for (let i=index; i < this.length; i++) {
-            this.listValue[i] = this.listValue[i+1]
-        }
-
-        delete this.listValue[this.length -1]
-        this.length--
-        return this.listValue
-    }
-
-    search(value) {
-        for(let i=0; i < this.length; i++) {
-            if (this.listValue[i] === value) {
-                return {'index' : i, 'value': this.listValue[i]}
-            }
-        }
-
-        return 'no element was found'
-        }
-    
-
-    modify(index, value) {
-        if (index > this.length || index < 0) {
-            return console.log("Incorrect index")
-        }
-
-        this.listValue[index] = value
-        return this.listValue
-    }
-}
-
-        </script>
-        <p>Произвожу пулл реквест в свой репозиторий</p>
-    </body>
-</html>
