a29e6783137362a666d2bc9a2e2146c4f07f361a
Ilya <ilya.krasnenkov@mail.ru>
2024-03-07 17:24:30 +0300

    Добавил типизацию для списка

--- ts-homework/list.ts
+++ ts-homework/list.ts
@@ -1,6 +1,6 @@
-class NewList {
+class NewList <T>{
     length: number;
-    listValue: { [key: number]: number };
+    listValue: { [key: number]: T };
 
     constructor() {
         this.length = 0;
@@ -11,13 +11,13 @@ class NewList {
         return this.length;
     }
 
-    add(value: number): { [key: number]: number } {
+    add(value: T): { [key: number]: T } {    
         this.listValue[this.length] = value;
         this.length++;
         return this.listValue;
     }
 
-    remove(index: number): { [key: number]: number } {
+    remove(index: number): { [key: number]: T } {
         if (index > this.length || index < 0) {
             console.log("Incorrect index");
             return {}
@@ -32,7 +32,7 @@ class NewList {
         return this.listValue;
     }
 
-    search(value: number): { index: number, value: number } | string {
+    search(value: T): { index: number, value: T } | string {
         for (let i = 0; i < this.length; i++) {
             if (this.listValue[i] === value) {
                 return { 'index': i, 'value': this.listValue[i] };
@@ -41,7 +41,7 @@ class NewList {
         return 'no element was found';
     }
 
-    modify(index: number, value: number): { [key: number]: number } {
+    modify(index: number, value: T): { [key: number]: T } {
         if (index > this.length || index < 0) {
             console.log("Incorrect index");
             return {}
@@ -64,3 +64,15 @@ console.log(myList)
 console.log(myList.search(0))
 myList.remove(3)
 console.log(myList)
+let myListString = new NewList<string>()
+myListString.add('4')
+myListString.add('sasa')
+myListString.add('zaza')
+myListString.add('100')
+myListString.add('12')
+console.log(myListString)
+myListString.modify(3,'0')
+console.log(myListString)
+console.log(myListString.search('0'))
+myListString.remove(3)
+console.log(myListString)
